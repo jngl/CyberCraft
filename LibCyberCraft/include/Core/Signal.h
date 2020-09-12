e4061c92edddd4e619b377265dbd96f44d512a3c
jngl <jngl92@gmail.com>
2020-09-12 12:08:18 +0200

    add Signal

---
+++ LibCyberCraft/include/Core/Signal.h
@@ -0,0 +1,39 @@
+//
+// Created by jngl on 12/09/2020.
+//
+
+#ifndef CYBERCRAFT_SIGNAL_H
+#define CYBERCRAFT_SIGNAL_H
+
+#include <vector>
+#include <functional>
+#include <iostream>
+
+template<class ...Args>
+class Signal
+{
+public:
+    using Func = std::function<void(Args...)>;
+
+    void connect(Func func){
+        m_functions.push_back(func);
+    }
+
+protected:
+    std::vector<Func> m_functions;
+};
+
+template<class ...Args>
+class SignalEmitter : public Signal<Args...>
+{
+public:
+    using Func = typename Signal<Args...>::Func;
+
+    void emit(Args... args){
+        for(Func& f: Signal<Args...>::m_functions){
+            f(args...);
+        }
+    }
+};
+
+#endif //CYBERCRAFT_SIGNAL_H
