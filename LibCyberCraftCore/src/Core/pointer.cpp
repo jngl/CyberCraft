e44f8481709b0b37ef4c546d804af4fcdfba4a87
jngl <jngl92@gmail.com>
2020-12-03 22:15:58 +0100

    add ccCore::Ref

---
+++ LibCyberCraftCore/src/Core/pointer.cpp
@@ -0,0 +1,11 @@
+//
+// Created by jngl on 03/12/2020.
+//
+
+#include "Core/pointer.h"
+
+namespace ccCore {
+    const char* NullPointerException::what() const noexcept {
+        return "Null Pointer Exception";
+    }
+}
