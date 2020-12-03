ba043e641690e4aa3695d311d1fef40cc50556be
jngl <jngl92@gmail.com>
2020-12-03 21:31:51 +0100

    better low level graphics

---
+++ LibCyberCraftSystem/include/System/DdsFile.h
@@ -0,0 +1,18 @@
+//
+// Created by jngl on 10/10/2020.
+//
+
+#ifndef CYBERCRAFT_DDSFILE_H
+#define CYBERCRAFT_DDSFILE_H
+
+#include "System/Common.h"
+
+#include "System/GraphicsData.h"
+
+#include <vector>
+
+namespace cc::System {
+    TextureData readDdsFile(std::string_view filename);
+}
+
+#endif //CYBERCRAFT_DDSFILE_H
