4dbda5b49d6c4881cea3d702754b972bb0efaec2
jngl <jngl92@gmail.com>
2020-12-05 16:06:31 +0100

    add cc::hash_32_fnv1a_const & c::hash_64_fnv1a_const & cc::typeName

---
+++ test/Test_typeName.cpp
@@ -0,0 +1,16 @@
+//
+// Created by jngl on 05/12/2020.
+//
+
+#include <catch.hpp>
+#include <Core/typeName.h>
+
+class Foo{};
+
+constexpr std::string_view intTypeName = cc::typeName<int>();
+constexpr std::string_view fooTypeName = cc::typeName<Foo>();
+
+TEST_CASE( "cc::typeName" ) {
+    REQUIRE(intTypeName == "int");
+    REQUIRE(fooTypeName == "Foo");
+}
