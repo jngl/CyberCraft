f59a54bab26195eec55fa3fb961cbd1ef8c49125
jngl <jngl92@gmail.com>
2021-07-10 17:04:34 +0200

    create lib kernel

---
+++ Lib/Kernel/include/Kernel/Tmp.h
@@ -0,0 +1,13 @@
+//
+// Created by jngl on 10/07/2021.
+//
+
+#ifndef CYBERCRAFT_TMP_H
+#define CYBERCRAFT_TMP_H
+
+namespace ck
+{
+    void tmp();
+}
+
+#endif //CYBERCRAFT_TMP_H
---
+++ Lib/Kernel/src/Tmp.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jngl on 10/07/2021.
+//
+
+#include "Kernel/Tmp.h"
+
+namespace ck{
+    void tmp();
+}
